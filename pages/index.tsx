fb19de23328a18417e4669291ffbac03b0aa6e5e
Timothy Broome <timothy@broome.me>
2025-08-03 11:43:59 +0100

    Storybook installation

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,10 +39,12 @@ export default function Home() {
           <ObjectOfSearch></ObjectOfSearch>
           <SearchType></SearchType>
           <RemovalOfMoreThanOuterClothing></RemovalOfMoreThanOuterClothing>
-          
+
         </div>
 
       </main>
+
+      
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
     
       </footer>
